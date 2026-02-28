d4dfaba59e35e2f008faa824b99ee69955d4e82e
Lokesh <lucky8595591003@gmail.com>
2026-02-28 16:26:59 +0530

    Fix DB for Railway

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ import os
 import socket
 
 app = Flask(__name__)
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SECRET_KEY"] = "supersecretkey"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Lokesh%40123@localhost:5432/socialx"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["UPLOAD_FOLDER"] = os.path.join("static", "uploads")
 app.config["MAX_CONTENT_LENGTH"] = 5 * 1024 * 1024
